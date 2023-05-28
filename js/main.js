92567cfeed10ddaec6bef4d55ff5d97f2ac3b162
Laman Isgandarova <104322290+laman-isg@users.noreply.github.com>
2023-05-29 00:35:00 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('input[type="checkbox"]').addEventListener("click", function(){
+    document.getElementById("night").classList.toggle("day");
+    document.getElementById("moon").classList.toggle("sun");
+  });
+  
